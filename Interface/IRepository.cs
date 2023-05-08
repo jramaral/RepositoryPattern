a97c38155da34689b04c65c7a0b41ee15cb46e3c
Jose Roberto <j.robertoamaral@gmail.com>
2023-05-08 15:46:33 -0400

    Init

---
+++ Interface/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Interface
+{
+    public interface IRepository<T> : IDisposable where T : class
+    {
+        T GetById(Guid id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+
+
+    }
+}
---
+++ Repository/Repository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryPattern.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity); 
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Remove(entity);
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(Guid id)
+        {
+          return (T)_context.Find(typeof(T),id)!;
+        }
+
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
